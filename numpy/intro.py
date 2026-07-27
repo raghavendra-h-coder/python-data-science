7d573d2ca4e6d1452ade0e3faa702e8345aee49d
venkataraghavendraraochatti <cvrr27894@gmail.com>
2026-07-27 22:37:47 +0530

    numpy

---
+++ numpy/intro.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+num_array = np.array([1,2,3,4,5,6,7,8,9])
+
+print(num_array)
+
+print(num_array - 5)
+
+print(num_array * 5)
+
+print(num_array / 5)
+
+print(num_array + 5)
+
+print(type(num_array))
+
+
+# practice
+
+student_marks_array = np.array([90,99,87,79,67,35,12,99,89,86,100])
+employee_salaries_array = np.array([144000, 200000, 320000, 360000, 750000, 850000, 1000000, 1400000, 1600000, 1900000, 3000000])
+family_members_ages_array = np.array([61, 59, 26, 32, 53, 46, 22, 1])
+print(student_marks_array)
+print(employee_salaries_array)
+print(family_members_ages_array)
+
+print(student_marks_array - 5)
+print(student_marks_array * 5)
+print(student_marks_array / 5)
+print(student_marks_array + 5)
+print(type(student_marks_array))
+
+print(employee_salaries_array - 5)
+print(employee_salaries_array * 5)
+print(employee_salaries_array / 5)
+print(employee_salaries_array + 5)
+print(type(employee_salaries_array))
+
+print(family_members_ages_array - 5)
+print(family_members_ages_array * 5)
+print(family_members_ages_array / 5)
+print(family_members_ages_array + 5)
+print(type(family_members_ages_array))
